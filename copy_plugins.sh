58cae85af905ac76063adaa22f64262175fb7aac
bad13 <97944036+bad13@users.noreply.github.com>
2022-01-18 12:10:50 +0100

    Add files via upload by myself

---
+++ copy_plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# create by Dennis Suhl
+
+# define variables
+site=$(omd sites --bare)
+version=$(omd version --bare)
+
+echo -e "\e[1;42m COPY PLUGIN \e[0m"
+sudo cp /opt/omd/versions/$version/share/check_mk/agents/plugins/mk_inventory.linux /opt/omd/sites/$site/local/share/check_mk/agents/plugins/
