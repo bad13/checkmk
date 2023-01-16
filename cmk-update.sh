08ff4ff80b6d638ababe655bc8dcbe8a5a2d11c1
bad13 <me@bad13.de>
2023-01-16 09:27:33 +0100

    add backup option to cmk-update.sh

--- cmk-update.sh
+++ cmk-update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# create by me
-# version 1.1 (30.01.2022)
+# create by Dennis Suhl
+# version 1.0 (18.01.2022) create
+# version 1.1 (24.08.2022) add omd cleanup
+# version 1.2 (26.08.2022) add omd backup
 
 # check the installed release and codename, define variables to build the url-string
-# current_release="$(lsb_release -rs)"# rs = release in short description
+#current_release="$(lsb_release -rs)"# rs = release in short description
 current_codename="$(lsb_release -cs)"_amd64.deb # cs = codename in short description
 site=$(omd sites --bare)
 current_version=$(omd version --bare)
@@ -13,8 +15,17 @@ echo -e
 echo -e "\e[1;46m INFO: Auf diesem Server läuft die Site $site in der Version $current_version\e[0m"
 echo -e
 
+# doing a backup before update
+echo -e "\e[1;42m DOING A BACKUP \e[0m"
+date=$(date +"%F")
+sudo touch backup-cmk-$current_version-$date
+sudo omd stop $site
+sudo omd backup $site backup-cmk-$current_version-$date
+sudo omd start $site
+sudo mv backup-cmk-$current_version-$date backup backup-cmk-$current_version-$date_latest
+
 # set patchlevel and sublevel for setting download urls
-patchlevel=6 # set this to your desired patchlevel
+patchlevel=10 # set this to your desired patchlevel
 subpatchlevel=_0 # set this to your desired subpatchlevel - normally always _0
 
 # download the desired cmk-server-version from cmk-website
@@ -23,7 +34,7 @@ wget https://download.checkmk.com/checkmk/2.1.0p$patchlevel/check-mk-raw-2.1.0p$
 
 # installation cmk-server raw edition
 echo -e "\e[1;42m INSTALL CHECKMK-SERVER \e[0m"
-apt install -y ./check-mk-raw-2.1.0p$patchlevel$subpatchlevel.$current_codename
+sudo apt install -y ./check-mk-raw-2.1.0p$patchlevel$subpatchlevel.$current_codename
 
 # stop the current running cmk-site
 echo -e "\e[1;42m STOP SITE \e[0m" $site
@@ -42,9 +53,12 @@ new_version=$(omd version --bare)
 
 # installation cmk-agent
 echo -e "\e[1;42m INSTALL CHECKMK-AGENT \e[0m"
-patchlevel=6 # set this to your desired patchlevel
+patchlevel=10
 cd /opt/omd/versions/$new_version/share/check_mk/agents
 sudo apt install -y ./check-mk-agent_2.1.0p$patchlevel-1_all.deb
+
+# cleanup installations
+sudo omd cleanup
 
 # giving some short infos
 echo -e
